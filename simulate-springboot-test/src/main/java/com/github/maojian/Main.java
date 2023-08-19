f369c2471fb1b163a086656e5429bc12575f7cd6
maojian_ohhey <maojian_ohhey@163.com>
2023-08-19 14:54:24 +0800

    模拟 springboot

--- simulate-springboot-test/src/main/java/com/github/maojian/Main.java
+++ simulate-springboot-test/src/main/java/com/github/maojian/Main.java
@@ -1,7 +1,8 @@
 package com.github.maojian;
 
+@SimulateSpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SimulateSpringApplication.run(Main.class);
     }
 }
