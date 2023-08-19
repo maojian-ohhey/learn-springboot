9d7f6a828b2f335360565081149d26c11cbe7be8
maojian_ohhey <maojian_ohhey@163.com>
2023-08-19 14:31:09 +0800

    pom.xml 修改

---
+++ simulate-springboot-test/src/main/java/com/github/maojian/service/TestService.java
@@ -0,0 +1,11 @@
+package com.github.maojian.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    public String test(){
+        return "success";
+    }
+}
---
+++ simulate-springboot/src/main/java/com/github/maojian/SimulateSpringApplication.java
@@ -0,0 +1,7 @@
+package com.github.maojian;
+
+public class SimulateSpringApplication {
+    public static void run(Class clazz){
+
+    }
+}
